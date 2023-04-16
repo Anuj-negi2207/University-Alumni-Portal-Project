57cddc21a1512c0a7d8bd574623e8024a223a31e
Anuj-negi2207 <pienegi22@gmail.com>
2023-04-16 23:23:45 +0530

    Integrated ChatBot

---
+++ static/base/app.js
@@ -0,0 +1,8 @@
+(function(d, m){
+    var kommunicateSettings = 
+        {"appId":"1ee8853a1ea1d37e3f2de66a21382f4f","popupWidget":true,"automaticChatOpenOnNavigation":true};
+    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+    s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+    var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+    window.kommunicate = m; m._globals = kommunicateSettings;
+})(document, window.kommunicate || {});
